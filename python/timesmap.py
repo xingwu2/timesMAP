8f1878794b00179080fa0a3721e428f550f46599
XingWu <wuxingtom@gmail.com>
2023-10-23 22:02:41 -0700

    Update timesmap.py

--- python/timesmap.py
+++ python/timesmap.py
@@ -33,15 +33,43 @@ C =  np.array(pd.read_csv(args.covar,sep="\t",header=None))
 if args.mode == 1:
 	trace,alpha_trace,beta_trace,gamma_trace = multi.sampling(y,C,X,1,1,0.001,12000,args.output)
 	trace.to_csv(args.output+"_multiplicative_trace.txt",sep="\t",header=False,index=False)
-	alpha_trace.to_csv(args.output+"_multiplicative_alpha_trace.txt",sep="\t",header=False,index=False)
-	beta_trace.to_csv(args.output+"_multiplicative_beta_trace.txt",sep="\t",header=False,index=False)
-	gamma_trace.to_csv(args.output+"_multiplicative_gamma_trace.txt",sep="\t",header=False,index=False)
+	
+	alpha_trace_avg = np.mean(alpha_trace,axis=0)
+	alpha_trace_sd = np.std(alpha_trace,axis = 0)
+	OUTPUT_ALPHA = open(args.output+"_multiplicative_alpha.txt","w")
+	for i in range(C.shape[1]):
+		print("%f\t%f" %(alpha_trace_avg[i],alpha_trace_sd[i]),file = OUTPUT_ALPHA)
+
+	beta_trace_avg = np.mean(beta_trace,axis=0)
+	beta_trace_sd = np.std(beta_trace,axis = 0)
+	OUTPUT_BETA = open(args.output+"_multiplicative_beta.txt","w")
+	for i in range(len(beta_trace_avg)):
+		print("%s\t%f\t%f" %(i,beta_trace_avg[i],beta_trace_sd[i]),file = OUTPUT_BETA)
+
+	pip = np.mean(gamma_trace,axis = 0)
+	OUTPUT_PIP = open(args.output+"_multiplicative_pip.txt","w")
+	for i in range(len(pip)):
+		print("%s\t%f" %(i,pip[i]),file = OUTPUT_PIP)
 
 elif args.mode == 2:
 	trace,alpha_trace,beta_trace,gamma_trace = add.sampling(y,C,X,1,1,0.001,12000,args.output)
 	trace.to_csv(args.output+"_additive_trace.txt",sep="\t",header=False,index=False)
-	alpha_trace.to_csv(args.output+"_additive_alpha_trace.txt",sep="\t",header=False,index=False)
-	beta_trace.to_csv(args.output+"_additive_beta_trace.txt",sep="\t",header=False,index=False)
-	gamma_trace.to_csv(args.output+"_additive_gamma_trace.txt",sep="\t",header=False,index=False)
+
+	alpha_trace_avg = np.mean(alpha_trace,axis=0)
+	alpha_trace_sd = np.std(alpha_trace,axis = 0)
+	OUTPUT_ALPHA = open(args.output+"_additive_alpha.txt","w")
+	for i in range(C.shape[1]):
+		print("%f\t%f" %(alpha_trace_avg[i],alpha_trace_sd[i]),file = OUTPUT_ALPHA)
+
+	beta_trace_avg = np.mean(beta_trace,axis=0)
+	beta_trace_sd = np.std(beta_trace,axis = 0)
+	OUTPUT_BETA = open(args.output+"_additive_beta.txt","w")
+	for i in range(len(beta_trace_avg)):
+		print("%s\t%f\t%f" %(i,beta_trace_avg[i],beta_trace_sd[i]),file = OUTPUT_BETA)
+
+	pip = np.mean(gamma_trace,axis = 0)
+	OUTPUT_PIP = open(args.output+"_additive_pip.txt","w")
+	for i in range(len(pip)):
+		print("%s\t%f" %(i,pip[i]),file = OUTPUT_PIP)
 
 
